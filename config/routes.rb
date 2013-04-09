1f89412d5d62ef5ebddac20e565b7e173f961d85
davidyxu <dvdyxu@gmail.com>
2013-04-08 21:11:13 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 WebStore::Application.routes.draw do
   resources :users
+  
+  # REV: I think it would be appropriate to have sessions be singular here
+  #      as only one session should exist at a time
   resources :sessions
   resources :products
   resource :cart
